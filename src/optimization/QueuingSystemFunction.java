8e6d202deebcefc4ca5858e4b3e3e78212925c24
Savvato <d.savvato@gmail.com>
2017-05-28 19:15:39 +0300

    ## QueuingSystemOptimization * calculating average response

--- src/optimization/QueuingSystemFunction.java
+++ src/optimization/QueuingSystemFunction.java
@@ -7,10 +7,28 @@ import system.QueuingSystem;
  */
 public class QueuingSystemFunction implements ResponseFunctionInterface
 {
+    private static final byte EXPERIMENTS_COUNT = 5;
+
     private double averageRequestIncomingTime;
 
     public QueuingSystemFunction(double averageRequestIncomingTime) {
         this.averageRequestIncomingTime = averageRequestIncomingTime;
+    }
+
+    /**
+     * Получение математического ожидания значения отклика в точке
+     *
+     * @param x1 serviceChannelsCount
+     * @param x2 averageServiceTime
+     * @return
+     */
+    @Override
+    public double calculate(double x1, double x2) {
+        double sumResult = 0;
+        for (int i = 0; i < EXPERIMENTS_COUNT; i++) {
+            sumResult += this.calculateSimpleResponse(x1, x2);
+        }
+        return sumResult / EXPERIMENTS_COUNT;
     }
 
     /**
@@ -20,8 +38,7 @@ public class QueuingSystemFunction implements ResponseFunctionInterface
      * @param x2 averageServiceTime
      * @return
      */
-    @Override
-    public double calculate(double x1, double x2) {
+    private double calculateSimpleResponse(double x1, double x2) {
         QueuingSystem queuingSystem = new QueuingSystem(
                 (long) this.averageRequestIncomingTime,
                 (int) x1,
